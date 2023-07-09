73567a510a06bfe29c1b86f57cfa069997dd1cd5
muhammad-jiku <muhammadjiku364@gmail.com>
2023-07-09 14:45:06 +0600

    404 page added

--- components/Shared/EmptyState.jsx
+++ components/Shared/EmptyState.jsx
@@ -2,16 +2,16 @@
 
 import React from 'react';
 import Link from 'next/link';
-import NotFoundImg from '@/assets/gifs/404.gif';
+import NotFoundImg from '@/assets/gifs/NotFound.gif';
 
 function EmptyState() {
   return (
-    <div className='hero min-h-full'>
+    <div className='hero min-h-screen'>
       <div className='hero-content text-center'>
-        <div className='max-w-md'>
+        <div className='max-w-md container mx-auto flex flex-col items-center'>
           <img src={NotFoundImg.src} alt='not found' />
           <h1 className='text-3xl font-bold py-4'>Page is not found!</h1>
-          <button className='btn bg-main text-white hover:bg-white hover:text-black hover:border-main mr-2 hidden lg:flex'>
+          <button className='btn bg-main text-white hover:bg-white hover:text-black hover:border-main mr-2'>
             <Link href={`/`} className=''>
               Go to Home
             </Link>
