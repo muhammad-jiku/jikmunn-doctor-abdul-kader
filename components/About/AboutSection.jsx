4168b28527cea6a43d46f99549adfc18696d465b
muhammad-jiku <muhammadjiku364@gmail.com>
2023-07-10 20:18:35 +0600

    Social icons component added

--- components/About/AboutSection.jsx
+++ components/About/AboutSection.jsx
@@ -1,10 +1,9 @@
 'use client';
 
 import React from 'react';
-import { FaFacebookF, FaInstagram, FaLinkedinIn } from 'react-icons/fa';
 import bgImg from '../../assets/images/bg1.jpeg';
 import myImg from '../../assets/images/me.png';
-import { CustomBanner } from '..';
+import { CustomBanner, SocialIcons } from '..';
 
 function AboutSection() {
   const bannerData = {
@@ -53,34 +52,7 @@ function AboutSection() {
               </p>
 
               <div className='p-6'>
-                <a
-                  href='https://www.facebook.com/ak.pin2'
-                  target='_blank'
-                  rel='noopener noreferrer'
-                >
-                  <button className='btn btn-circle btn-sm md:btn-md mr-2 hover:bg-info hover:text-white'>
-                    <FaFacebookF className='text-lg md:text-2xl' />
-                  </button>
-                </a>
-                <a
-                  href='https://www.instagram.com/abdulkaderpintu/'
-                  target='_blank'
-                  rel='noopener noreferrer'
-                >
-                  <button className='btn btn-circle btn-sm md:btn-md mr-2 hover:bg-gradient-to-r from-purple-500 via-pink-500 to-red-500 hover:text-white'>
-                    <FaInstagram className='text-lg md:text-2xl' />
-                  </button>
-                </a>
-
-                <a
-                  href='https://www.linkedin.com/in/dr-abdul-kader-3297b3124/'
-                  target='_blank'
-                  rel='noopener noreferrer'
-                >
-                  <button className='btn btn-circle btn-sm md:btn-md mr-2 hover:bg-info hover:text-white'>
-                    <FaLinkedinIn className='text-lg md:text-2xl' />
-                  </button>
-                </a>
+                <SocialIcons />
               </div>
             </div>
           </div>
