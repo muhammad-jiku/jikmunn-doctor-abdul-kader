a492769cb835d03601396e9ff5d018e42296264f
muhammad-jiku <muhammadjiku364@gmail.com>
2023-07-11 19:21:23 +0600

    timeline section added

--- components/About/AboutSection.jsx
+++ components/About/AboutSection.jsx
@@ -3,7 +3,18 @@
 import React from 'react';
 import bgImg from '../../assets/images/bg1.jpeg';
 import myImg from '../../assets/images/me.png';
+import '../../styles/timeline.css';
 import { CustomBanner, SocialIcons } from '..';
+import {
+  VerticalTimeline,
+  VerticalTimelineElement,
+} from 'react-vertical-timeline-component';
+import 'react-vertical-timeline-component/style.min.css';
+import {
+  MdWorkOutline,
+  MdOutlineSchool,
+  MdOutlineStarBorderPurple500,
+} from 'react-icons/md';
 
 function AboutSection() {
   const bannerData = {
@@ -25,37 +36,168 @@ function AboutSection() {
             explore my timeline
           </h2>
           <hr className='w-1/3 lg:w-1/4 mb-2 border-2 border-slate-300' />
-          <div className='flex flex-col w-full lg:flex-row justify-center items-center container mx-auto my-10'>
-            <div className='grid flex-grow card place-items-center w-full lg:w-1/2'>
-              <img className='mask mask-circle h-56 w-48' src={myImg.src} />
-            </div>
-            <div className='divider lg:divider-horizontal'></div>
-            <div className='grid flex-grow card place-items-start w-full lg:w-1/2 p-2 lg:p-10'>
-              <p className='p-6 text-justify text-sm lg:text-lg'>
-                Hello there, I am{' '}
-                <span className='text-main'>
-                  Dr. Muhammad Abdul Kader Pintu
-                </span>
-                . Welcome to my portfolio. I&apos;ve designed and developed web
-                applications with HTML5, CSS3, JavaScript, Bootstrap, Tailwind
-                CSS, ReactJS, NodeJS, ExpressJS, MongoDB for 3+ years. I am also
-                accomplished in tools like Netlify, Heroku, Firebase, Git,
-                Github, Figma, Canvas, and Chrome Dev Tools.
-              </p>
-
-              <p className='p-6 text-justify text-sm lg:text-lg'>
-                I&apos;ve also built web applications with StripeJS, React
-                Stripe, React-firebase-hooks, React-router, React-bootstrap, and
-                DaisyUI. I love to work with MERN Stack. I like to call myself
-                &ldquo;a passionate MERN Stack Web Developer&rdquo;. In my spare
-                time, I watch Football, Movies.
-              </p>
-
+          <div className='flex flex-col w-full items-center container mx-auto my-10'>
+            <div className='grid flex-grow card place-items-center'>
+              <div className='avatar mx-2'>
+                <div className='w-52 rounded-full ring ring-main ring-offset-base-100 ring-offset-2'>
+                  <img src={myImg.src} alt='avatar' />
+                </div>
+              </div>{' '}
               <div className='p-6'>
                 <SocialIcons />
               </div>
             </div>
+            <div className='divider'></div>
+            <div className='grid flex-grow card place-items-start p-0 md:p-2'>
+              <p className='p-6 text-justify text-sm lg:text-lg'>
+                Hello there, I am{' '}
+                <span className='text-main'>Dr. Abdul Kader</span>. Welcome
+                here. I completed my M.B.B.S from Cumilla Medical College and
+                obtain the degree of Bachelor of Medicine and Bachelor of
+                Surgery from the University of Chattogram. After that, I
+                completed my scheduled one-year internship training direct
+                supervision of the professor in charge of the Dept. of Medicine,
+                Surgery and Obstetrics, Gynecology.
+              </p>
+
+              <p className='p-6 text-justify text-sm lg:text-lg'>
+                I received the certificate of full registration as a medical
+                practitioner. And I have then, completed a three-month course of
+                Certificate in Medical Ultrasound (C.M.U). I have completed the
+                six-month certificate course on the Diabetology under Distine
+                Learning Programme (DLP). I Passed the 39th BCS (Health) &
+                attended Orientation training developed by DGHS and joined as an
+                Assitant Surgeon. In my spare time, I watch Football matches,
+                and Movies.
+              </p>
+
+              {/* <div className='p-6'>
+                <SocialIcons />
+              </div> */}
+            </div>
           </div>
+
+          <VerticalTimeline lineColor='#000'>
+            {/* 1st */}
+            <VerticalTimelineElement
+              className='vertical-timeline-element--education'
+              date='2016'
+              iconStyle={{ background: 'rgb(233, 30, 99)', color: '#fff' }}
+              icon={<MdOutlineSchool />}
+            >
+              <h3 className='vertical-timeline-element-title'>M.B.B.S</h3>
+              <h4 className='vertical-timeline-element-subtitle'>
+                Cumilla Medical College, Cumilla, Bangladesh
+              </h4>
+              {/* <p>Strategy, Social Media</p> */}
+            </VerticalTimelineElement>
+            {/* 2nd */}
+            <VerticalTimelineElement
+              className='vertical-timeline-element--work'
+              date='2016-2017'
+              // iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+              // icon={<MdWorkOutline />}
+              iconStyle={{ background: 'rgb(233, 30, 99)', color: '#fff' }}
+              icon={<MdOutlineSchool />}
+            >
+              <h3 className='vertical-timeline-element-title'>
+                Internship Training Programme
+              </h3>
+              <h4 className='vertical-timeline-element-subtitle'>
+                Cumilla Medical College, Cumilla, Bangladesh
+              </h4>
+              <p>
+                Under the direct supervision of the professor in charge of the
+                Dept. of Medicine, Surgery and Obstetrics, Gynecology.
+              </p>
+            </VerticalTimelineElement>
+            {/* 3rd */}
+            <VerticalTimelineElement
+              className='vertical-timeline-element--work'
+              date='2017'
+              iconStyle={{ background: 'rgb(233, 30, 99)', color: '#fff' }}
+              icon={<MdOutlineSchool />}
+            >
+              <h3 className='vertical-timeline-element-title'>B.M.D.C </h3>
+              <h4 className='vertical-timeline-element-subtitle'>
+                Los Angeles, CA
+              </h4>
+              <p>User Experience, Visual Design</p>
+            </VerticalTimelineElement>
+            {/* 4th */}
+            <VerticalTimelineElement
+              className='vertical-timeline-element--work'
+              date='2006 - 2008'
+              iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+              icon={<MdWorkOutline />}
+            >
+              <h3 className='vertical-timeline-element-title'>Web Designer</h3>
+              <h4 className='vertical-timeline-element-subtitle'>
+                San Francisco, CA
+              </h4>
+              <p>User Experience, Visual Design</p>
+            </VerticalTimelineElement>
+            {/* 5th */}
+            <VerticalTimelineElement
+              className='vertical-timeline-element--work'
+              contentStyle={{
+                background: 'rgb(33, 150, 243)',
+                color: '#fff',
+              }}
+              contentArrowStyle={{
+                borderRight: '7px solid  rgb(33, 150, 243)',
+              }}
+              date='2011 - present'
+              dateClassName='dateInfo'
+              iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+              icon={<MdWorkOutline />}
+            >
+              <h3 className='vertical-timeline-element-title'>
+                Creative Director
+              </h3>
+              <h4 className='vertical-timeline-element-subtitle'>Miami, FL</h4>
+              <p>
+                Creative Direction, User Experience, Visual Design, Project
+                Management, Team Leading
+              </p>
+            </VerticalTimelineElement>
+
+            {/* 6th */}
+            <VerticalTimelineElement
+              className='vertical-timeline-element--education'
+              date='November 2012'
+              iconStyle={{ background: 'rgb(233, 30, 99)', color: '#fff' }}
+              icon={<MdOutlineSchool />}
+            >
+              <h3 className='vertical-timeline-element-title'>
+                Agile Development Scrum Master
+              </h3>
+              <h4 className='vertical-timeline-element-subtitle'>
+                Certification
+              </h4>
+              <p>Creative Direction, User Experience, Visual Design</p>
+            </VerticalTimelineElement>
+            {/* 7th */}
+            <VerticalTimelineElement
+              className='vertical-timeline-element--education'
+              date='2002 - 2006'
+              iconStyle={{ background: 'rgb(233, 30, 99)', color: '#fff' }}
+              icon={<MdOutlineSchool />}
+            >
+              <h3 className='vertical-timeline-element-title'>
+                Bachelor of Science in Interactive Digital Media Visual Imaging
+              </h3>
+              <h4 className='vertical-timeline-element-subtitle'>
+                Bachelor Degree
+              </h4>
+              <p>Creative Direction, Visual Design</p>
+            </VerticalTimelineElement>
+            {/* final */}
+            <VerticalTimelineElement
+              iconStyle={{ background: 'rgb(16, 204, 82)', color: '#fff' }}
+              icon={<MdOutlineStarBorderPurple500 />}
+            />
+          </VerticalTimeline>
         </div>
       </div>
     </>
