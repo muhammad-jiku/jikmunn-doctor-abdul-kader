81c14f0f15808febcb3a193028300fe0baa0f0f7
muhammad-jiku <muhammadjiku364@gmail.com>
2023-07-14 10:59:53 +0600

    database connection

---
+++ backend/utils/connectToDB.js
@@ -0,0 +1,25 @@
+import mongoose from 'mongoose';
+
+let isConnected = false; // track the connection
+
+export const connectToDB = async () => {
+  mongoose.set('strictQuery', true);
+
+  if (isConnected) {
+    console.log('DB is already connected!');
+    return;
+  }
+
+  try {
+    await mongoose.connect(process.env.DB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+
+    isConnected = true;
+    console.log('DB connected!!');
+  } catch (error) {
+    console.log('DB disconnected!!');
+    console.log(error);
+  }
+};
