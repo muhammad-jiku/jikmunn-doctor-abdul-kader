98e7cbcec03f6320f1665f3661d82e6d2ab3506d
muhammad-jiku <muhammadjiku364@gmail.com>
2023-07-14 18:11:03 +0600

    global provider added to layout component

--- components/Layout/MainLayout.jsx
+++ components/Layout/MainLayout.jsx
@@ -2,13 +2,16 @@
 
 import React, { Suspense } from 'react';
 import { Navbar, Footer } from '..';
+import { GlobalProvider } from '@/provider/GlobalProvider';
 
 function MainLayout() {
   return (
     <div className='flex flex-col min-h-screen overflow-x-hidden'>
-      <Navbar />
-      <Suspense fallback={<></>}>{children}</Suspense>
-      <Footer />
+      <GlobalProvider>
+        <Navbar />
+        <Suspense fallback={<></>}>{children}</Suspense>
+        <Footer />
+      </GlobalProvider>
     </div>
   );
 }
