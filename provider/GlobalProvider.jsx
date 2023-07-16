b8eb4398ec8bb031000662ea4b95c15c83cb6bf1
muhammad-jiku <muhammadjiku364@gmail.com>
2023-07-16 08:05:23 +0600

    toast added

--- provider/GlobalProvider.jsx
+++ provider/GlobalProvider.jsx
@@ -1,11 +1,14 @@
 'use client';
 
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import { AuthProvider } from '@/context/AuthContext';
 import { SessionProvider } from 'next-auth/react';
 
 export function GlobalProvider({ children }) {
   return (
     <>
+      <ToastContainer position='bottom-right' />
       <AuthProvider>
         <SessionProvider>{children}</SessionProvider>
       </AuthProvider>
