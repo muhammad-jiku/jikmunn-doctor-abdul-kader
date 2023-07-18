c66363ceb45a278d3d13d59fb2d7d972f4c06d71
muhammad-jiku <muhammadjiku364@gmail.com>
2023-07-18 21:58:17 +0600

    user appointment lists

---
+++ components/Dashboard/User/Appointments.jsx
@@ -0,0 +1,53 @@
+'use client';
+
+import React from 'react';
+
+function Appointments() {
+  const appointmentLists = [
+    {
+      id: '01',
+      name: 'Diabetes Treatment',
+      paymentStatus: 'Paid',
+      date: '07/16/2023',
+    },
+    {
+      id: '02',
+      name: 'Diabetes Treatment',
+      paymentStatus: 'Unpaid',
+      date: '07/11/2023',
+    },
+  ];
+
+  return (
+    <div className='container mx-auto my-4 p-2 flex flex-col items-center'>
+      <h2 className='text-3xl lg:text-5xl font-bold font-lobster text-main tracking-wider mt-2 mb-4'>
+        My Appointments
+      </h2>
+      <hr className='w-1/3 lg:w-1/4 mb-2 border-2 border-slate-300' />
+      <div className='overflow-x-auto'>
+        <table className='table table-xs'>
+          <thead>
+            <tr>
+              <th>No.</th>
+              <th>Name</th>
+              <th>Status</th>
+              <th>Last Meet</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <th>1</th>
+              <td>Cy Ganderton</td>
+              <td>Quality Control Specialist</td>
+              <td>Littel, Schaden and Vandervort</td>
+              <td>12/16/2020</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+}
+
+export default Appointments;
