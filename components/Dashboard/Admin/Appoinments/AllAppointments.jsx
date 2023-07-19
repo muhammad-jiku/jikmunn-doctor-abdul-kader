3b6c18e0ab6919444ab9f1908169d644b6d64ba5
muhammad-jiku <muhammadjiku364@gmail.com>
2023-07-19 21:34:20 +0600

    admin appointments modifies

--- components/Dashboard/Admin/Appoinments/AllAppointments.jsx
+++ components/Dashboard/Admin/Appoinments/AllAppointments.jsx
@@ -1,9 +1,50 @@
 'use client';
 
 import React from 'react';
+import AppointmentsRow from './AppointmentsRow';
 
 function AllAppointments() {
-  return <div>AllAppointments</div>;
+  const appointmentLists = [
+    {
+      id: '01',
+      name: 'Diabetes Treatment',
+      paymentStatus: 'Paid',
+      date: '07/16/2023',
+    },
+    {
+      id: '02',
+      name: 'Diabetes Treatment',
+      paymentStatus: 'Unpaid',
+      date: '07/11/2023',
+    },
+  ];
+
+  return (
+    <div className='container mx-auto my-4 p-2 flex flex-col items-center'>
+      <h2 className='text-3xl lg:text-5xl font-bold font-lobster text-main tracking-wider mt-2 mb-4'>
+        All Prices
+      </h2>
+      <hr className='w-1/3 lg:w-1/4 mb-2 border-2 border-slate-300' />
+      <div className='overflow-x-auto my-2 w-full'>
+        <table className='table table-xs sm:table-sm md:table-md lg:table-lg p-1 md:p-2 w-full'>
+          <thead>
+            <tr>
+              <th>No.</th>
+              <th>Name</th>
+              <th>Status</th>
+              <th>Last Meet</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {appointmentLists?.map((a, idx) => (
+              <AppointmentsRow key={idx} a={a} />
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 }
 
 export default AllAppointments;
