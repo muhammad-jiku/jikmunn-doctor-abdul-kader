c44f618a3d8a3f96ee20ac5a9f7f7fd2f868cb8b
muhammad-jiku <muhammadjiku364@gmail.com>
2023-07-19 13:05:17 +0600

    admin layout component

--- app/dashboard/admin/layout.jsx
+++ app/dashboard/admin/layout.jsx
@@ -1,11 +1,11 @@
 import { Sidebar } from '@/components';
 
-export default function UserLayout({ children }) {
+export default function AdminLayout({ children }) {
   return (
     <>
       <section className='py-5 sm:py-7 bg-blue-100'>
         <div className='container max-w-screen-xl mx-auto px-4'>
-          <h1 className='text-bold text-2xl'>My Dashboard</h1>
+          <h1 className='text-bold text-2xl'>Admin Dashboard</h1>
         </div>
       </section>
 
