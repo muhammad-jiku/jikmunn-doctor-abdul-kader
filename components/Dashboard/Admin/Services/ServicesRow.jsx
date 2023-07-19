b137b813556027610d0341f93934a4e24a8d0b5a
muhammad-jiku <muhammadjiku364@gmail.com>
2023-07-19 13:26:58 +0600

    all services and row component added

---
+++ components/Dashboard/Admin/Services/ServicesRow.jsx
@@ -0,0 +1,31 @@
+'use client';
+
+import React from 'react';
+import { AiOutlineEdit, AiFillDelete } from 'react-icons/ai';
+
+function ServicesRow({ a }) {
+  return (
+    <tr>
+      <th>{a?.id}</th>
+      <th>{a?.name}</th>
+      <th>{a?.paymentStatus}</th>
+      <th>{a?.date}</th>
+      <th className='flex'>
+        <label
+          htmlFor='delete-modal'
+          className='btn btn-error bg-green-600 text-white font-bold mr-2'
+        >
+          <AiOutlineEdit className='text-lg' />
+        </label>
+        <label
+          htmlFor='delete-modal'
+          className='btn btn-error bg-red-600 text-white font-bold'
+        >
+          <AiFillDelete className='text-lg' />
+        </label>
+      </th>
+    </tr>
+  );
+}
+
+export default ServicesRow;
