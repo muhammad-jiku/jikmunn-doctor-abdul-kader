1994c94e1be7b6f5052c8c0d6fc2e328f6b025d3
muhammad-jiku <muhammadjiku364@gmail.com>
2023-07-20 12:59:38 +0600

    update profile controller func

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,12 +1,14 @@
 import User from '../models/User';
+import fs from 'fs';
 
+// create account
 export const registerUser = async (req, res) => {
   try {
     const { username, email, password } = await req.body;
 
     const user = await User.create({
-      username,
-      email,
+      username: username.toLowerCase(),
+      email: email.toLowerCase(),
       password,
     });
 
@@ -14,6 +16,40 @@ export const registerUser = async (req, res) => {
       success: true,
       data: user,
       message: 'User created successfully',
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      success: false,
+      message: 'Something went wrong',
+    });
+  }
+};
+
+// update profile
+export const updateProfile = async (req, res) => {
+  try {
+    const { avatar, username, email, phone, country, state, city, address } =
+      await req.body;
+
+    const newUserData = await {
+      username: username.toLowerCase(),
+      email: email.toLowerCase(),
+      phone,
+      country,
+      state,
+      city,
+      address,
+    };
+
+    //
+
+    const user = await User.findByIdAndUpdate(req.user._id, newUserData);
+
+    return res.status(200).json({
+      success: true,
+      data: user,
+      message: 'User updated successfully',
     });
   } catch (error) {
     console.log(error);
