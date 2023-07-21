182319e1fc5b377098f1de51de668b44b89662c7
muhammad-jiku <muhammadjiku364@gmail.com>
2023-07-21 13:05:52 +0600

    role based dashboard sidebar view

--- components/Dashboard/Sidebar.jsx
+++ components/Dashboard/Sidebar.jsx
@@ -35,71 +35,71 @@ function Sidebar() {
           </Link>
         </li>
         <hr className='my-2 border-2 border-slate-300' />
-        {/* {user?.role === 'admin' && ( */}
-        <>
-          <li>
-            <Link
-              href='/dashboard/admin/new/service'
-              className='block px-3 py-2 text-gray hover:bg-blue-100 hover:text-main rounded-md'
-            >
-              New Service
-            </Link>
-          </li>
+        {user?.role === 'admin' && (
+          <>
+            <li>
+              <Link
+                href='/dashboard/admin/new/service'
+                className='block px-3 py-2 text-gray hover:bg-blue-100 hover:text-main rounded-md'
+              >
+                New Service
+              </Link>
+            </li>
 
-          <li>
-            <Link
-              href='/dashboard/admin/services'
-              className='block px-3 py-2 text-gray hover:bg-blue-100 hover:text-main rounded-md'
-            >
-              All Services
-            </Link>
-          </li>
-          <li>
-            <Link
-              href='/dashboard/admin/new/price'
-              className='block px-3 py-2 text-gray hover:bg-blue-100 hover:text-main rounded-md'
-            >
-              New Price
-            </Link>
-          </li>
+            <li>
+              <Link
+                href='/dashboard/admin/services'
+                className='block px-3 py-2 text-gray hover:bg-blue-100 hover:text-main rounded-md'
+              >
+                All Services
+              </Link>
+            </li>
+            <li>
+              <Link
+                href='/dashboard/admin/new/price'
+                className='block px-3 py-2 text-gray hover:bg-blue-100 hover:text-main rounded-md'
+              >
+                New Price
+              </Link>
+            </li>
 
-          <li>
-            <Link
-              href='/dashboard/admin/prices'
-              className='block px-3 py-2 text-gray hover:bg-blue-100 hover:text-main rounded-md'
-            >
-              All Prices
-            </Link>
-          </li>
+            <li>
+              <Link
+                href='/dashboard/admin/prices'
+                className='block px-3 py-2 text-gray hover:bg-blue-100 hover:text-main rounded-md'
+              >
+                All Prices
+              </Link>
+            </li>
 
-          <li>
-            <Link
-              href='/dashboard/admin/users'
-              className='block px-3 py-2 text-gray hover:bg-blue-100 hover:text-main rounded-md'
-            >
-              All Users
-            </Link>
-          </li>
+            <li>
+              <Link
+                href='/dashboard/admin/users'
+                className='block px-3 py-2 text-gray hover:bg-blue-100 hover:text-main rounded-md'
+              >
+                All Users
+              </Link>
+            </li>
 
-          <li>
-            <Link
-              href='/dashboard/admin/appointments'
-              className='block px-3 py-2 text-gray hover:bg-blue-100 hover:text-main rounded-md'
-            >
-              All Appointments
-            </Link>
-          </li>
+            <li>
+              <Link
+                href='/dashboard/admin/appointments'
+                className='block px-3 py-2 text-gray hover:bg-blue-100 hover:text-main rounded-md'
+              >
+                All Appointments
+              </Link>
+            </li>
 
-          <li>
-            <Link
-              href='/dashboard/admin/reviews'
-              className='block px-3 py-2 text-gray hover:bg-blue-100 hover:text-main rounded-md'
-            >
-              All Reviews
-            </Link>
-          </li>
-        </>
-        {/* )} */}
+            <li>
+              <Link
+                href='/dashboard/admin/reviews'
+                className='block px-3 py-2 text-gray hover:bg-blue-100 hover:text-main rounded-md'
+              >
+                All Reviews
+              </Link>
+            </li>
+          </>
+        )}
       </ul>
     </aside>
   );
